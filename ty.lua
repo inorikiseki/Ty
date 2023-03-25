65798a9fa5078f01b84c9cafee87db16712ceed8
inorikiseki <2531319647@qq.com>
2023-03-25 14:09:29 +0800

    ty.lua mod

--- ty.lua
+++ ty.lua
@@ -1,9 +1,24 @@
 -- * ty des
 
 file = io.open("script.ty","r")
-print(file:read("a"))
-
+src = file:read("a")
 -- load src
+
+
+cur = 1
+line = 1
+
+function check()
+	if letter() then 
+		print("*"..peek().."*")
+	else 
+		print("_"..peek().."_")
+	end
+end
+
+for i,char in pair(src) do
+	check()
+end
 
 function token()
 	if(letter()) then
@@ -11,5 +26,42 @@ function token()
 	end
 end
 
+function number()
+	return peek()>=string.char('0')
+	and peek()<=string.char('9')
+end
+
 function letter()
-	if(
+	return peek()>=string.char('a')
+	and peek()<=string.char('z')
+	and peek()>=string.char('A')
+	and peek()>=string.char('Z')
+end
+
+function peek()
+	return string.sub(src,cur,cur)
+end
+
+function advance()
+	cur = cur + 1
+	if peek() == string.char('\n') then
+		line = line +1
+	return peek()
+	end
+end
+
+function err(msg,line)
+	print("[ERROR at "..line.."]"..msg)
+end
+
+
+
+
+
+
+
+
+
+
+
+
